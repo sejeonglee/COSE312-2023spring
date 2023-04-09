9be1399f14f962e6d2b63b283939062804b58f12
sejeonglee <sejeonglee2@gmail.com>
2023-04-09 17:01:07 +0900

    hw2

--- homework/hw2/parser/parse.ml
+++ homework/hw2/parser/parse.ml
@@ -4,83 +4,280 @@ type symbol = T of string | N of string | Epsilon | End
 type production = symbol * symbol list
 type cfg = symbol list * symbol list * symbol * production list
 
-let string_of_symbol s = 
-  match s with 
-  | T x -> x 
-  | N x -> x 
-  | Epsilon -> "epsilon"
-  | End -> "$"
+let string_of_symbol s =
+  match s with T x -> x | N x -> x | Epsilon -> "epsilon" | End -> "$"
 
-let string_of_prod (lhs, rhs) = 
-    string_of_symbol lhs ^ " -> " ^ 
-      string_of_list ~first:"" ~last:"" ~sep:" " string_of_symbol rhs 
+let string_of_prod (lhs, rhs) =
+  string_of_symbol lhs ^ " -> "
+  ^ string_of_list ~first:"" ~last:"" ~sep:" " string_of_symbol rhs
 
-module FIRST = struct 
+module FIRST = struct
   type t = (symbol, symbol BatSet.t) BatMap.t
 
-  let empty = BatMap.empty 
-  
-  let find : symbol -> t -> symbol BatSet.t 
-  =fun s t -> try BatMap.find s t with _ -> BatSet.empty 
-  
-  let add : symbol -> symbol -> t -> t 
-  =fun s1 s2 t -> BatMap.add s1 (BatSet.add s2 (find s1 t)) t
-  
-  let add_set : symbol -> symbol BatSet.t -> t -> t 
-  =fun s1 ss t -> BatSet.fold (fun s2 -> add s1 s2) ss t
+  let empty = BatMap.empty
 
-  let tostring : t -> string
-  =fun t -> 
-    BatMap.foldi (fun s ss str -> 
-      str ^ string_of_symbol s ^ " |-> " ^ string_of_set string_of_symbol ss ^ "\n"
-    ) t ""
-end   
+  let find : symbol -> t -> symbol BatSet.t =
+   fun s t -> try BatMap.find s t with _ -> BatSet.empty
+
+  let add : symbol -> symbol -> t -> t =
+   fun s1 s2 t -> BatMap.add s1 (BatSet.add s2 (find s1 t)) t
+
+  let add_set : symbol -> symbol BatSet.t -> t -> t =
+   fun s1 ss t -> BatSet.fold (fun s2 -> add s1 s2) ss t
+
+  let tostring : t -> string =
+   fun t ->
+    BatMap.foldi
+      (fun s ss str ->
+        str ^ string_of_symbol s ^ " |-> "
+        ^ string_of_set string_of_symbol ss
+        ^ "\n")
+      t ""
+end
 
 module FOLLOW = struct
-  type t = (symbol, symbol BatSet.t) BatMap.t 
+  type t = (symbol, symbol BatSet.t) BatMap.t
 
-  let empty = BatMap.empty 
+  let empty = BatMap.empty
 
-  let find : symbol -> t -> symbol BatSet.t 
-  =fun s t -> try BatMap.find s t with _ -> BatSet.empty 
+  let find : symbol -> t -> symbol BatSet.t =
+   fun s t -> try BatMap.find s t with _ -> BatSet.empty
 
-  let add : symbol -> symbol -> t -> t 
-  =fun s1 s2 t -> BatMap.add s1 (BatSet.add s2 (find s1 t)) t
+  let add : symbol -> symbol -> t -> t =
+   fun s1 s2 t -> BatMap.add s1 (BatSet.add s2 (find s1 t)) t
 
-  let add_set : symbol -> symbol BatSet.t -> t -> t 
-  =fun s1 ss t -> BatSet.fold (fun s2 -> add s1 s2) ss t
+  let add_set : symbol -> symbol BatSet.t -> t -> t =
+   fun s1 ss t -> BatSet.fold (fun s2 -> add s1 s2) ss t
 
-  let tostring : t -> string
-  =fun t -> 
-    BatMap.foldi (fun s ss str -> 
-      str ^ string_of_symbol s ^ " |-> " ^ string_of_set string_of_symbol ss ^ "\n"
-    ) t ""
+  let tostring : t -> string =
+   fun t ->
+    BatMap.foldi
+      (fun s ss str ->
+        str ^ string_of_symbol s ^ " |-> "
+        ^ string_of_set string_of_symbol ss
+        ^ "\n")
+      t ""
 end
 
 module ParsingTable = struct
-  type t = (symbol * symbol, (symbol * symbol list) BatSet.t) BatMap.t 
+  type t = (symbol * symbol, (symbol * symbol list) BatSet.t) BatMap.t
 
-  let empty = BatMap.empty 
+  let empty = BatMap.empty
 
-  let find (nonterm, term) t = try BatMap.find (nonterm, term) t with _ -> BatSet.empty 
+  let find (nonterm, term) t =
+    try BatMap.find (nonterm, term) t with _ -> BatSet.empty
 
-  let add (nonterm, term) prod t = 
+  let add (nonterm, term) prod t =
     BatMap.add (nonterm, term) (BatSet.add prod (find (nonterm, term) t)) t
-    
-  let tostring : t -> string 
-  =fun t -> 
-    BatMap.foldi (fun (nonterm, term) prods str -> 
-      str ^ string_of_symbol nonterm ^ ", " ^ string_of_symbol term ^ " |-> " ^
-        string_of_set string_of_prod prods ^ "\n"
-    ) t ""
-    
-  let foldi = BatMap.foldi 
 
+  let tostring : t -> string =
+   fun t ->
+    BatMap.foldi
+      (fun (nonterm, term) prods str ->
+        str ^ string_of_symbol nonterm ^ ", " ^ string_of_symbol term ^ " |-> "
+        ^ string_of_set string_of_prod prods
+        ^ "\n")
+      t ""
+
+  let foldi = BatMap.foldi
   let for_all = BatMap.for_all
 end
 
-let check_LL1 : cfg -> bool
-=fun _ -> false (* TODO *)
+let compute_first_set : cfg -> FIRST.t =
+ fun grammar ->
+  let _, terms, _, prods = grammar in
+  let update first_set =
+    List.fold_left
+      (fun set prod ->
+        let lhs, rhs = prod in
+        let rec traverse_prod_rhs (rhs : symbol list) : FIRST.t =
+          match rhs with
+          | [] -> FIRST.add lhs Epsilon set
+          | s :: ss ->
+              FIRST.add_set lhs
+                (BatSet.remove Epsilon (FIRST.find s set))
+                (if not (BatSet.find_opt Epsilon (FIRST.find s set) = None) then
+                   traverse_prod_rhs ss
+                 else set)
+        in
 
-let parse : cfg -> symbol list -> bool
-=fun _ _ -> false (* TODO *)
+        traverse_prod_rhs rhs)
+      first_set prods
+  in
+  let initial_first_set =
+    List.fold_left
+      (fun set symbol -> FIRST.add symbol symbol set)
+      FIRST.empty
+      (terms @ [ Epsilon; End ])
+  in
+  let current_first_set = ref (update initial_first_set) in
+  let break_flag = ref true in
+  while !break_flag do
+    let updated_first_set = update !current_first_set in
+    if FIRST.tostring !current_first_set = FIRST.tostring updated_first_set then
+      break_flag := false
+    else ();
+    current_first_set := updated_first_set
+  done;
+  !current_first_set
+
+let compute_follow_set : cfg -> FOLLOW.t =
+ fun grammar ->
+  let _, _, start, prods = grammar in
+  let first_set = compute_first_set grammar in
+  let initial_follow_set = FOLLOW.add start End FOLLOW.empty in
+  let rec get_first_of_str (beta : symbol list) =
+    match beta with
+    | [] -> BatSet.singleton Epsilon
+    | s :: ss ->
+        let first_s = FIRST.find s first_set in
+        if not (BatSet.find_opt Epsilon first_s = None) then
+          BatSet.union (BatSet.remove Epsilon first_s) (get_first_of_str ss)
+        else first_s
+  in
+  let update follow_set =
+    List.fold_left
+      (fun fold_set prod ->
+        let a, rhs = prod in
+        let rec traverse_prod_rhs (rhs : symbol list) (set : FOLLOW.t) :
+            FOLLOW.t =
+          match rhs with
+          | [] -> set
+          | b :: beta -> (
+              match b with
+              | N _ ->
+                  if List.length beta = 0 then
+                    FOLLOW.add_set b (FOLLOW.find a set) set (* CASE 3*)
+                  else
+                    let first_beta = get_first_of_str beta in
+                    let case2_set =
+                      FOLLOW.add_set b (BatSet.remove Epsilon first_beta) set
+                    in
+                    let case4_set =
+                      if not (BatSet.find_opt Epsilon first_beta = None) then
+                        FOLLOW.add_set b (FOLLOW.find a case2_set) case2_set
+                      else case2_set
+                    in
+                    traverse_prod_rhs beta case4_set
+              | _ -> traverse_prod_rhs beta set)
+        in
+        traverse_prod_rhs rhs fold_set)
+      follow_set prods
+  in
+  let current_follow_set = ref (update initial_follow_set) in
+  let break_flag = ref true in
+  while !break_flag do
+    let updated_follow_set = update !current_follow_set in
+    if FOLLOW.tostring !current_follow_set = FOLLOW.tostring updated_follow_set
+    then break_flag := false
+    else ();
+    current_follow_set := updated_follow_set
+  done;
+  !current_follow_set
+
+let construct_parsing_table : cfg -> ParsingTable.t =
+ fun grammar ->
+  let _, _, _, prods = grammar in
+  let first_set = compute_first_set grammar in
+  let follow_set = compute_follow_set grammar in
+  let rec get_first_of_str (alpha : symbol list) =
+    match alpha with
+    | [] -> BatSet.singleton Epsilon
+    | s :: ss ->
+        let first_s = FIRST.find s first_set in
+        if not (BatSet.find_opt Epsilon first_s = None) then
+          BatSet.union (BatSet.remove Epsilon first_s) (get_first_of_str ss)
+        else first_s
+  in
+  let rec traverse_prods (prods : production list) (table : ParsingTable.t) :
+      ParsingTable.t =
+    match prods with
+    | [] -> table
+    | prod :: tl ->
+        let a, alpha = prod in
+        let first_alpha = get_first_of_str alpha in
+
+        let case1_table =
+          BatSet.fold
+            (fun s t ->
+              if BatSet.is_empty (ParsingTable.find (a, s) t) then
+                ParsingTable.add (a, s) prod t
+              else raise (Failure "Not LL1"))
+            (BatSet.remove Epsilon first_alpha)
+            table
+        in
+        let follow_A = FOLLOW.find a follow_set in
+        let case2_1_table =
+          if not (BatSet.find_opt Epsilon first_alpha = None) then
+            BatSet.fold
+              (fun b t ->
+                if BatSet.is_empty (ParsingTable.find (a, b) t) then
+                  ParsingTable.add (a, b) prod t
+                else raise (Failure "Not LL1"))
+              follow_A case1_table
+          else case1_table
+        in
+        (if
+           (not (BatSet.find_opt Epsilon first_alpha = None))
+           && not (BatSet.find_opt End follow_A = None)
+         then ParsingTable.add (a, End) prod case2_1_table
+         else case2_1_table)
+        |> traverse_prods tl
+  in
+  traverse_prods prods ParsingTable.empty
+
+let check_LL1 : cfg -> bool =
+ fun grammar ->
+  (* print_string (FIRST.tostring (compute_first_set grammar)); *)
+  (* print_string (FOLLOW.tostring (compute_follow_set grammar)); *)
+  (* print_string (ParsingTable.tostring (construct_parsing_table grammar)); *)
+  try
+    ignore (construct_parsing_table grammar);
+    true
+  with Failure _ -> false
+
+(* TODO *)
+
+let parse : cfg -> symbol list -> bool =
+ fun grammar str ->
+  let _, _, start, _ = grammar in
+  let parsing_table = construct_parsing_table grammar in
+  match str with
+  | [] -> true
+  | first_w :: _ -> (
+      let a = ref first_w in
+      let stack = ref [ start ] in
+      let w = ref str in
+      try
+        while not (List.length !stack = 0) do
+          (* print_string
+               ("stack: "
+               ^ string_of_list string_of_symbol !stack
+               ^ "  | input: "
+               ^ string_of_list string_of_symbol !w
+               ^ "  | a: " ^ string_of_symbol !a);
+             print_newline (); *)
+          match !stack with
+          | [] -> ()
+          | x :: tl -> (
+              if x = !a then (
+                stack := tl;
+                w := List.tl !w;
+                a := List.hd !w)
+              else
+                match x with
+                | T _ -> raise (Failure "Parse Error")
+                | _ ->
+                    (if
+                       BatSet.is_empty (ParsingTable.find (x, !a) parsing_table)
+                     then raise (Failure "Parse Error")
+                     else
+                       let prod =
+                         BatSet.choose (ParsingTable.find (x, !a) parsing_table)
+                       in
+                       let _, rhs = prod in
+                       stack := rhs @ tl);
+                    ())
+        done;
+        true
+      with Failure _ -> false)
--- homework/hw2/regex2dfa/trans.ml
+++ homework/hw2/regex2dfa/trans.ml
@@ -1,19 +1,158 @@
-open Regex 
+open Regex
 
 exception Not_implemented
 
-let regex2nfa : Regex.t -> Nfa.t 
-=fun _ -> raise Not_implemented (* TODO *)
+let regex2nfa : Regex.t -> Nfa.t =
+ fun input_regex ->
+  let rec process (regex : Regex.t) : Nfa.t =
+    let empty_nfa = Nfa.create_new_nfa () in
+    match regex with
+    | Empty ->
+        let new_state, new_nfa = Nfa.create_state empty_nfa in
+        Nfa.add_final_state new_nfa new_state
+    | Epsilon ->
+        let state, nfa = Nfa.create_state empty_nfa in
+        let nfa = Nfa.add_final_state nfa state in
+        let nfa = Nfa.add_epsilon_edge nfa (Nfa.get_initial_state nfa, state) in
+        nfa
+    | Alpha a ->
+        let state, nfa = Nfa.create_state empty_nfa in
+        let nfa = Nfa.add_final_state nfa state in
+        let nfa = Nfa.add_edge nfa (Nfa.get_initial_state nfa, a, state) in
+        nfa
+    | OR (r1, r2) ->
+        let nfa_r1 = process r1 in
+        let nfa_r2 = process r2 in
+        let initial1 = Nfa.get_initial_state nfa_r1 in
+        let initial2 = Nfa.get_initial_state nfa_r2 in
+        let final1 = BatSet.to_list (Nfa.get_final_states nfa_r1) in
+        let final2 = BatSet.to_list (Nfa.get_final_states nfa_r2) in
+        let initial = Nfa.get_initial_state empty_nfa in
+        let final, nfa = Nfa.create_state empty_nfa in
+        let nfa = Nfa.add_final_state nfa final in
+        let nfa = Nfa.add_states nfa (Nfa.get_states nfa_r1) in
+        let nfa = Nfa.add_states nfa (Nfa.get_states nfa_r2) in
+        let nfa = Nfa.add_edges nfa (Nfa.get_edges nfa_r1) in
+        let nfa = Nfa.add_edges nfa (Nfa.get_edges nfa_r2) in
+        let nfa =
+          Nfa.add_edges nfa
+            ( [],
+              [ (initial, initial1); (initial, initial2) ]
+              @ List.map (fun s -> (s, final)) (final1 @ final2) )
+        in
+        nfa
+    | CONCAT (r1, r2) ->
+        let nfa_r1 = process r1 in
+        let nfa_r2 = process r2 in
+        let initial1 = Nfa.get_initial_state nfa_r1 in
+        let initial2 = Nfa.get_initial_state nfa_r2 in
+        let final1 = BatSet.to_list (Nfa.get_final_states nfa_r1) in
+        let final2 = BatSet.to_list (Nfa.get_final_states nfa_r2) in
+        let initial = Nfa.get_initial_state empty_nfa in
+        let final, nfa = Nfa.create_state empty_nfa in
+        let nfa = Nfa.add_final_state nfa final in
+        let nfa = Nfa.add_states nfa (Nfa.get_states nfa_r1) in
+        let nfa = Nfa.add_states nfa (Nfa.get_states nfa_r2) in
+        let nfa = Nfa.add_edges nfa (Nfa.get_edges nfa_r1) in
+        let nfa = Nfa.add_edges nfa (Nfa.get_edges nfa_r2) in
+        let nfa =
+          Nfa.add_edges nfa
+            ( [],
+              [ (initial, initial1) ]
+              @ List.map (fun s -> (s, initial2)) final1
+              @ List.map (fun s -> (s, final)) final2 )
+        in
+        nfa
+    | STAR r ->
+        let nfa_r = process r in
+        let initial_r = Nfa.get_initial_state nfa_r in
+        let final_r = BatSet.to_list (Nfa.get_final_states nfa_r) in
+        let initial = Nfa.get_initial_state empty_nfa in
+        let final, nfa = Nfa.create_state empty_nfa in
+        let nfa = Nfa.add_final_state nfa final in
+        let nfa = Nfa.add_states nfa (Nfa.get_states nfa_r) in
+        let nfa = Nfa.add_edges nfa (Nfa.get_edges nfa_r) in
+        let nfa =
+          Nfa.add_edges nfa
+            ( [],
+              [ (initial, initial_r); (initial, final) ]
+              @ List.map (fun s -> (s, initial_r)) final_r
+              @ List.map (fun s -> (s, final)) final_r )
+        in
+        nfa
+  in
 
-let nfa2dfa : Nfa.t -> Dfa.t
-=fun _ -> raise Not_implemented (* TODO *)
- 
+  process input_regex
+
+let nfa2dfa : Nfa.t -> Dfa.t =
+ fun nfa ->
+  let rec epsilon_closure (nfa : Nfa.t) (states : Nfa.states) : Dfa.state =
+    let cur_states = states in
+    let next_states =
+      BatSet.fold
+        (fun nfa_state dfa_states ->
+          BatSet.union (Nfa.get_next_state_epsilon nfa nfa_state) dfa_states)
+        states states
+    in
+    if BatSet.equal cur_states next_states then cur_states
+    else epsilon_closure nfa next_states
+  in
+  let d0 = epsilon_closure nfa (BatSet.singleton (Nfa.get_initial_state nfa)) in
+  let d = ref (BatSet.singleton d0) in
+  let dfa = ref (Dfa.create_new_dfa d0) in
+  let w = ref (BatSet.singleton d0) in
+  if not (BatSet.disjoint d0 (Nfa.get_final_states nfa)) then
+    dfa := Dfa.add_final_state !dfa d0;
+  while not (BatSet.is_empty !w) do
+    let q = BatSet.any !w in
+    w := BatSet.remove q !w;
+    let ta =
+      epsilon_closure nfa
+        (BatSet.fold
+           (fun nfa_state t ->
+             BatSet.union (Nfa.get_next_state nfa nfa_state Regex.A) t)
+           q BatSet.empty)
+    in
+    if not (BatSet.exists (fun s -> s = ta) !d) (*D*) then w := BatSet.add ta !w
+    else ();
+    d := BatSet.add ta !d;
+    if BatSet.disjoint ta (Nfa.get_final_states nfa) then
+      dfa := Dfa.add_state !dfa ta
+    else dfa := Dfa.add_final_state !dfa ta;
+    dfa := Dfa.add_edge !dfa (q, Regex.A, ta);
+    let tb =
+      epsilon_closure nfa
+        (BatSet.fold
+           (fun nfa_state t ->
+             BatSet.union (Nfa.get_next_state nfa nfa_state Regex.B) t)
+           q BatSet.empty)
+    in
+    if not (BatSet.exists (fun s -> s = tb) !d) (*D*) then w := BatSet.add tb !w
+    else ();
+    d := BatSet.add tb !d;
+    if BatSet.disjoint tb (Nfa.get_final_states nfa) then
+      dfa := Dfa.add_state !dfa tb
+    else dfa := Dfa.add_final_state !dfa tb;
+    dfa := Dfa.add_edge !dfa (q, Regex.B, tb)
+  done;
+  !dfa
+
 (* Do not modify this function *)
-let regex2dfa : Regex.t -> Dfa.t
-=fun regex -> 
+let regex2dfa : Regex.t -> Dfa.t =
+ fun regex ->
   let nfa = regex2nfa regex in
   let dfa = nfa2dfa nfa in
-    dfa
+  dfa
 
-let run_dfa : Dfa.t -> alphabet list -> bool
-=fun _ _ -> raise Not_implemented (* TODO *)
+let run_dfa : Dfa.t -> alphabet list -> bool =
+ fun dfa s_list ->
+  let rec run_dfa' : Dfa.state -> alphabet list -> bool =
+   fun state s_list ->
+    match s_list with
+    | [] -> Dfa.is_final_state dfa state
+    | hd :: tl ->
+        let next_state = Dfa.get_next_state dfa state hd in
+        if BatSet.is_empty next_state then false else run_dfa' next_state tl
+  in
+  run_dfa' (Dfa.get_initial_state dfa) s_list
+(* TODO *)
